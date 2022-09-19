76e930fb2c1fd9095db7f21fd97a0a41f49e7f3e
Muizah <mapampa7@gmail.com>
2022-09-19 02:16:26 +0100

    finish desktop view

--- script.js
+++ script.js
@@ -2,12 +2,42 @@ const featuresBtn = document.getElementById("features");
 const companyBtn = document.getElementById("company");
 const featuresDropdown = document.getElementById("sub_features");
 const companyDropdown = document.getElementById("sub_company");
-const arrowDown = document.getElementById("arrow_down");
+const arrows = document.getElementsByClassName("arrow_down");
 
-featuresBtn.addEventListener("click", () => {
+featuresBtn.addEventListener("mouseover", () => {
   featuresDropdown.style.display = "flex";
+  Array.from(arrows).forEach((arrow) => {
+    arrow.setAttribute("src", "images/icon-arrow-up.svg");
+  });
 });
 
-companyBtn.addEventListener("click", () => {
+companyBtn.addEventListener("mouseover", () => {
   companyDropdown.style.display = "flex";
+  Array.from(arrows).forEach((arrow) => {
+    arrow.setAttribute("src", "images/icon-arrow-up.svg");
+  });
+});
+
+document.addEventListener("mouseout", function (event) {
+  const isClickInsideFeaturesDropdown = featuresDropdown.contains(event.target);
+  const isClickInsideCompanyDropdown = companyDropdown.contains(event.target);
+  const isClickOutsideCompanyBtn = !companyBtn.contains(event.target);
+  const isClickOutsideFeaturesBtn = !featuresBtn.contains(event.target);
+  const isClickOutsideFeaturesDropdown = !isClickInsideFeaturesDropdown;
+  const isClickOutsideCompanyDropdown = !isClickInsideCompanyDropdown;
+
+  if (isClickOutsideFeaturesBtn && isClickOutsideFeaturesDropdown) {
+    featuresDropdown.style.display = "none";
+    Array.from(arrows).forEach((arrow) => {
+      arrow.setAttribute("src", "images/icon-arrow-down.svg");
+    });
+  }
+
+  if (isClickOutsideCompanyBtn && isClickOutsideCompanyDropdown) {
+    companyDropdown.style.display = "none";
+    Array.from(arrows).forEach((arrow) => {
+      arrow.setAttribute("src", "images/icon-arrow-down.svg");
+    });
+  }
+  console.log(isClickOutsideFeaturesBtn, isClickOutsideFeaturesDropdown);
 });
